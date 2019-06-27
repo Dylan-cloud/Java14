e34f89453abbccb213af70003b3847c7f5f7660e
19858 <1985875396@qq.com>
2019-06-27 22:49:30 +0800

    day1 Device by jin-yuan

---
+++ 6.27（设备）/java/com/cskaoyan/mapper/Device_MaintainMapper.java
@@ -0,0 +1,21 @@
+package com.cskaoyan.mapper;
+
+import com.cskaoyan.bean.Device_Maintain;
+
+import java.util.List;
+
+public interface Device_MaintainMapper {
+    int deleteByPrimaryKey(String deviceMaintainId);
+
+    int insert(Device_Maintain record);
+
+    int insertSelective(Device_Maintain record);
+
+    Device_Maintain selectByPrimaryKey(String deviceMaintainId);
+
+    List<Device_Maintain> selectAll();
+
+    int updateByPrimaryKeySelective(Device_Maintain record);
+
+    int updateByPrimaryKey(Device_Maintain record);
+}
